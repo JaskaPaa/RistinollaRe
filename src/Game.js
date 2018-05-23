a5de29f5aa514be4e494d057b0f559aa9b5b8584
Jaska <jaska.paakkonen@gmail.com>
2018-05-23 14:33:12 +0300

    Some updates

--- src/Game.js
+++ src/Game.js
@@ -21,12 +21,23 @@ class Game extends React.Component {
 
 class Board extends React.Component {
 
+
   componentDidMount() {
     this.updateCanvas();
   }
+  
+  handleClick = (e) => {
+    e.preventDefault();
+    console.log('The link was clicked.');
+    const ctx = this.refs.board.getContext('2d');
+    ctx.fillStyle = 'rgb(255, 165, ' + Math.floor(Math.random() * 255) +')';
+    ctx.fillRect(e.clientX, e.clientY, 100, 100);
+
+  }
+
 
   updateCanvas() {
-    const ctx = this.refs.canvas.getContext('2d');
+    const ctx = this.refs.board.getContext('2d');
     ctx.fillRect(0,0, 100, 100);
     ctx.fillRect(200,200, 100, 100);
   }
@@ -34,7 +45,7 @@ class Board extends React.Component {
   render() {
     return(
       <div>
-      <canvas ref="canvas" width={640} height={425} />
+      <canvas ref="board" width={500} height={500} onClick={this.handleClick} />
       </div>
     )
   }
